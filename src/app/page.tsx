058d21aecdcb44b2e7fdd7686b8f1a54156f6591
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-04-20 01:32:34 -0300

    criado component para produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from "@/types/ProductType";
+import { Product } from "./Components/Product";
 
 async function getProducts() {
   const res = await fetch("https://fakestoreapi.com/products");
@@ -9,7 +10,8 @@ async function getProducts() {
 }
 export default async function Home() {
   const products = await getProducts();
-  //console.log(products);
+  //if don't work, execute:
+  //console.log(products); 
   return (
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:p-0">
       <div
@@ -20,7 +22,7 @@ export default async function Home() {
       lg:grid-cols-4 gap-10
       xl:gap-6">
         {products.map((product: ProductType) => (
-          <div key={product.id}>{product.title} </div>
+          <Product key={product.id} product={product}></Product>
         ))}
       </div>
       <h1>senacx</h1>
