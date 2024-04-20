13970b1462ae93c62952bd543202c99089cf381f
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-04-20 01:22:01 -0300

    criado tipagem para produtos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
 import { Navbar } from "./Components/Navbar";
 
 export const metadata: Metadata = {
@@ -16,10 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        <main className="bg-slate-700 h-screen p-16">
-          {children}
-        </main>
+        <Navbar />
+        <main className="bg-slate-700 h-screen p-16">{children}</main>
       </body>
     </html>
   );
