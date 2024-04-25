45eeaf6d3bb9b1bf614617bc9f908b14cdc1aeb8
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-04-25 00:11:23 -0300

    add responsive bug fix

--- src/app/Components/AddCart.tsx
+++ src/app/Components/AddCart.tsx
@@ -28,7 +28,7 @@ export default function Product({ product }: { product: ProductType }) {
   return (
     <button
       onClick={() => handleAddToCart()}
-      className="rounded-md bg-orange-500 text-white px-3.5 py-2.5 text-sm text-center"
+      className="rounded-md bg-orange-500 text-white px-3.5 py-2.5 text-sm text-center hover:bg-orange-400 transition-transform duration-300"
     >
       Add to cart
     </button>
--- src/app/Components/Swiper.tsx
+++ src/app/Components/Swiper.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-
+import Image from "next/image";
 import banner1 from "@/../public/assets/images/Banner1.jpeg";
 import banner2 from "@/../public/assets/images/Banner2.jpg";
 import banner3 from "@/../public/assets/images/banner3.jpg";
-
 import {
   Navigation,
   Pagination,
@@ -16,37 +14,29 @@ import {
   Keyboard,
   Autoplay,
 } from "swiper/modules";
-import Image from "next/image";
 
 export default function App() {
   return (
-    <>
-      <div className="mt-16 pt-10 h-full">
-        <Swiper
-          cssMode={true}
-          navigation={true}
-          pagination={true}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
-          className="w-2/3 h-96 swiper"
-        >
-          <SwiperSlide className="flex items-center swiper-slide object-scale-down bg-black">
-            <Image
-              src={banner1}
-              alt="banner1"
-              layout="fill"
-              objectFit="cover"
-            />
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide object-fill w-full bg-red-500">
-            <Image src={banner2} alt="banner2" layout="fill" />
-          </SwiperSlide>
-          <SwiperSlide className="swiper-slide flex object-cover bg-blue-500">
-            <Image src={banner3} alt="banner3" layout="fill" objectFit="fill" />
-          </SwiperSlide>
-        </Swiper>
-      </div>
-    </>
+    <div className=" mt-16 pt-10">
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+        className="w-full md:w-2/3 h-96 swiper"
+      >
+        <SwiperSlide className="flex items-center swiper-slide">
+          <Image src={banner1} alt="banner1" layout="fill" objectFit="fill" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <Image src={banner2} alt="banner2" layout="fill" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide flex">
+          <Image src={banner3} alt="banner3" layout="fill" objectFit="fill" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
