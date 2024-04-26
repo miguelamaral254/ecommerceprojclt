2f7233564f5801a2ecc3bbfc66d2a594bc76e9f7
Miguel Amaral <118134009+miguelamaral254@users.noreply.github.com>
2024-04-26 00:42:52 -0300

    add responsivity to  CartDrawer

--- src/app/Components/CartDrawer.tsx
+++ src/app/Components/CartDrawer.tsx
@@ -21,15 +21,15 @@ export default function CartDrawer() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       onClick={() => useStore.toggleCart()}
-      className="fixed w-full h-screen bg-black/25 left-0 top-0 z-50"
+      className="fixed w-full h-screen bg-black/25 left-0 top-0 z-50 overflow-y-auto"
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className="absolute bg-slate-600 right-0 top-0 w-1/3 h-screen p-8 overflow-y-scroll"
+        className="absolute bg-slate-600 right-0 top-0 w-full md:w-1/3 h-screen p-8 overflow-y-scroll"
       >
         <button
           onClick={() => useStore.toggleCart()}
-          className="font-bold text-sm border border-white py-2 px-3 text-white"
+          className="font-bold text-sm border border-white py-2 px-3 text-white mb-4"
         >
           Back to store
         </button>
@@ -47,33 +47,39 @@ export default function CartDrawer() {
                     initial={{ scale: 0.5, rotateZ: -10, opacity: 0 }}
                     exit={{ scale: 0.5, rotateZ: -10, opacity: 0 }}
                     key={item.id}
-                    className="flex gap-4 py-4"
+                    className="flex flex-col md:flex-row gap-4 py-4"
                   >
-                    <Image
-                      src={item.image}
-                      alt={item.name}
-                      width={120}
-                      height={120}
-                      className="object-cover w-24"
-                    />
-                    <div>
-                      <h2 className="w-42 text-white truncate">{item.name}</h2>
+                    <div className="md:w-24 md:self-start">
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={120}
+                        height={120}
+                        className="object-cover w-full"
+                      />
+                    </div>
+                    <div className="flex-1">
+                      <h2 className="text-white truncate md:mt-0">
+                        {item.name}
+                      </h2>
                       <h2>Quantity: {item.quantity}</h2>
                       <p className="text-teal-600 text-sm font-bold">
                         {formatPrice(item.price)}
                       </p>
-                      <button
-                        className="py-1 px-2 border text-white rounded-md mt-2 text-sm mr-1"
-                        onClick={() => useStore.addProduct(item)}
-                      >
-                        Add +
-                      </button>
-                      <button
-                        onClick={() => useStore.removeProduct(item)}
-                        className="py-1 px-2 border text-white rounded-md mt-2 text-sm"
-                      >
-                        Remove -
-                      </button>
+                      <div className="mt-2 flex flex-wrap">
+                        <button
+                          className="py-1 px-2 border text-white rounded-md mt-2 text-sm mr-1"
+                          onClick={() => useStore.addProduct(item)}
+                        >
+                          Add +
+                        </button>
+                        <button
+                          onClick={() => useStore.removeProduct(item)}
+                          className="py-1 px-2 border text-white rounded-md mt-2 text-sm"
+                        >
+                          Remove -
+                        </button>
+                      </div>
                     </div>
                   </motion.div>
                 ))}
--- src/app/Components/NoItems.tsx
+++ src/app/Components/NoItems.tsx
@@ -1,9 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
 const NoItemsMessage = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <p className="text-white">No items yet</p>
+    <div className="flex w-full flex-col items-center justify-center h-full">
+      <h2 className="text-white font-sans text-6xl p-2">No items found</h2>
+      <p className="text-gray-400 p-2 text-lg">Looks like you haven't add any items yet</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-20 w-20 text-gray-400"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
     </div>
   );
 };
